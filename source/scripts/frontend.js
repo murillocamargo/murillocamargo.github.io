84c68d27778adf4eed5f0a97f1ecba40a632cd57
Murillo Camargo <murillo@murillocamargo.com.br>
2018-08-06 19:47:48 -0300

    Final styles

--- source/scripts/frontend.js
+++ source/scripts/frontend.js
@@ -7,9 +7,9 @@
         var fullpageAnimation = function () {
 
             $('#fullpage').fullpage({
-                sectionsColor: ['#0F2980', '#ffd200', '#c1c1c1'],
+                // sectionsColor: ['#0F2980', '#ffd200', '#c1c1c1'],
                 anchors: ['firstSection', 'secondSection', 'thirdSection'],
-                menu: '.main-nav ul',
+                menu: '.main-nav ul'
             });
         };
 
